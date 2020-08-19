523715e04ee1e20e71c0bb74c640ec70f00e1d55
Ryan Valizan <ryan@vnetkc.com>
2020-08-19 08:39:57 -0500

    :tada::sparkles: new highlight row feature
    This is loading now, but needs more css selector work

---
+++ src/extension/features/reports/report-striped-rows/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'ReportStripedRows',
+  type: 'checkbox',
+  default: false,
+  section: 'reports',
+  title: 'Striped Report Rows',
+  description: 'Shows a light gray background on every other category row.',
+};
